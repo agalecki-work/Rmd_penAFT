7a625bf7de7c4e002eca766b10838f6133acf8fb
agalecki <agalecki@umich.edu>
2022-08-03 10:49:14 -0400

    v-01

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rmd_penAFT repository
+
+Initial relesae August 3, 2022 (v-01)
---
+++ _02stich_Rscripts.R
@@ -0,0 +1,10 @@
+# ---
+
+stitch("99-test-chunks.R")
+
+stitch_rhtml("99-test-chunks.R")
+
+
+# lapply(as.list(R_nms), source)
+ 
+
