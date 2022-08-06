b3dc380cb9eac05920b66f296933f26042927c37
agalecki <agalecki@umich.edu>
2022-08-06 17:19:40 -0400

    }} in11-data-Rmd

--- _01process_Rmd.R
+++ _01process_Rmd.R
@@ -7,7 +7,7 @@ Rmd_names <- c("11-data",
                "14-fit",
                "90-test-chunks"
               )
-Rmd_names <- "90-test-chunks"
+##Rmd_names <- "90-test-chunks"
 lapply(as.list(Rmd_names), process_Rmd)
  
 
